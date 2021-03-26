be234d90cd700dd5600a05e5a2012037c5366049
Mohamed Mahmoud <mmahmoud@redhat.com>
2021-03-26 06:44:53 -0400

    added udp

--- iperf3-test/iperf3.sh
+++ iperf3-test/iperf3.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-set -eu
+set +x
+
+NAMESPACE=default
 
 cd $(dirname $0)
-
 function setup() {
 	oc create -f iperf3.yaml
 
-	until $(oc get pods -l app=iperf3-server -o jsonpath='{.items[0].status.containerStatuses[0].ready}'); do
+	until $(oc get pods -n ${NAMESPACE} -l app=iperf3-server -o jsonpath='{.items[0].status.containerStatuses[0].ready}'); do
     	echo "Waiting for iperf3 server to start..."
     	sleep 5
 	done
@@ -15,14 +16,20 @@ function setup() {
 }
 
 function run() {
-	CLIENTS=$(oc get pods -l app=iperf3-client -o name | cut -d'/' -f2)
+	CLIENTS=$(oc get pods -n ${NAMESPACE} -l app=iperf3-client -o name | cut -d'/' -f2)
 
 	for POD in ${CLIENTS}; do
-    	until $(oc get pod ${POD} -o jsonpath='{.status.containerStatuses[0].ready}'); do
+	counter=0
+    	until [[ $(oc get pod ${POD} -n ${NAMESPACE} -o jsonpath='{.status.containerStatuses[0].ready}') && $counter -lt 5 ]]; do
         	echo "Waiting for ${POD} to start..."
         	sleep 5
+		counter=$((counter+1))
     	done
-    	oc exec -it ${POD} -- bash -c 'iperf3 -c "$IPERF3_SERVER_SERVICE_HOST" -p "$IPERF3_SERVER_SERVICE_PORT" -t1'
+
+	if [[ $counter -lt 5 ]]; then
+    	    oc exec -it ${POD} -n ${NAMESPACE} -- bash -c 'iperf3 -c "$IPERF3_SERVER_SERVICE_HOST" -p "$IPERF3_SERVER_SERVICE_PORT_TCP"'
+    	    #oc exec -it ${POD} -n ${NAMESPACE} -- bash -c 'iperf3 -u -c "$IPERF3_SERVER_SERVICE_HOST" -p "$IPERF3_SERVER_SERVICE_PORT_UDP"'
+	fi
 
     	echo
 	done
