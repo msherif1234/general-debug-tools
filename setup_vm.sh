2fc2c89d98e6d000cb5e3f3b7833b8644040d452
Mohamed Mahmoud <mmahmoud@redhat.com>
2023-03-27 09:34:42 -0400

    updated dockerfile to include gdb tools
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- setup_vm.sh
+++ setup_vm.sh
@@ -40,7 +40,7 @@ function setup_fedora_vm() {
 	sudo systemctl enable libvirtd
 
 	#Install kind
-	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
+	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
 	chmod +x ./kind
 	sudo mv ./kind /usr/local/bin
 
