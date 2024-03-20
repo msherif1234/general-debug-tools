6b9e79b63621ed4252c318cbdc53abcaf7a4da1b
Mohamed Mahmoud <mmahmoud@redhat.com>
2024-03-20 07:01:33 -0400

    add bpftop to dbg pod
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- update-go.sh
+++ update-go.sh
@@ -5,7 +5,10 @@ mkdir -p $HOME/go/src
 git clone https://github.com/udhos/update-golang.git
 	
 pushd update-golang
-sudo RELEASE=1.20.10 ./update-golang.sh # if need to install latest remove RELEASE=xxx
+#sudo RELEASE=1.20.10 ./update-golang.sh # if need to install latest remove RELEASE=xxx
+sudo RELEASE=1.21.7 ./update-golang.sh # if need to install latest remove RELEASE=xxx
+#sudo RELEASE=1.22.1 ./update-golang.sh # if need to install latest remove RELEASE=xxx
+#sudo ./update-golang.sh # if need to install latest remove RELEASE=xxx
 popd
 	
 export GOPATH=$HOME/go
